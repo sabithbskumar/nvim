4db72008005ec3f1b2c6100b479e6596723a5405
sabithbskumar <65082465+sabithbskumar@users.noreply.github.com>
2025-01-22 22:50:39 +0530

    feat: add lazy.nvim setup for plugin management and theme configuration

--- init.lua
+++ init.lua
@@ -12,5 +12,11 @@ require("options")
 -- [[ Basic Keymaps ]]
 require("keymaps")
 
+-- [[ Install `lazy.nvim` plugin manager ]]
+require("lazy-bootstrap")
+
+-- [[ Configure and install plugins ]]
+require("lazy-plugins")
+
 -- Vim `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
---
+++ lua/lazy-plugins.lua
@@ -0,0 +1,37 @@
+-- [[ Configure and install plugins ]]
+--
+--  To check the current status of the plugins, run
+--    :Lazy
+--
+--  To update plugins run
+--    :Lazy update
+--
+require("lazy").setup({
+  -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
+  -- NOTE: Plugins can also be added by using a table,
+  -- with the first argument being the link and the following
+  -- keys can be used to configure plugin behavior/loading/etc.
+  --
+  -- Use `opts = {}` to force a plugin to be loaded.
+  require("plugins.theme"),
+}, {
+  ui = {
+    icons = vim.g.use_nerd_font and {} or {
+      cmd = "⌘",
+      config = "🛠",
+      event = "📅",
+      ft = "📂",
+      init = "⚙",
+      keys = "🗝",
+      plugin = "🔌",
+      runtime = "💻",
+      require = "🌙",
+      source = "📄",
+      start = "🚀",
+      task = "📌",
+      lazy = "💤 ",
+    },
+  },
+})
+
+-- vim: ts=2 sts=2 sw=2 et
